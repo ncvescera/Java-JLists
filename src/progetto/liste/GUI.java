4942649e6037d2229211d82f95dc5b4ffe819eaa
Nicolò Vescera <slayercandy@icloud.com>
2016-04-18 16:45:51 +0200

    Aggiunta barra per inserimento directory

--- src/progetto/liste/GUI.java
+++ src/progetto/liste/GUI.java
@@ -1,12 +1,25 @@
 package progetto.liste;
 
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class GUI implements ActionListener{
     JFrame frame = new JFrame("Progetto-Liste");
@@ -20,10 +33,15 @@ public class GUI implements ActionListener{
     JButton copia = new JButton("Copia");
     JButton taglia = new JButton("Taglia");
     
+    JPanel files = new JPanel();
+    JTextField target = new JTextField();
+    JButton submit = new JButton("Cerca");
+    
     public GUI() {
-        initModUno();
+        //initModUno();
         initModDue();
         initCopia();
+        initTarget();
         initTaglia();
         initBtnPanel();
         initFrame();    
@@ -54,12 +72,20 @@ public class GUI implements ActionListener{
         taglia.addActionListener(this);
     }
     
+    private void initTarget(){
+        target.setActionCommand("cerca");
+        target.addActionListener(this);
+    }
+    
     private void initFrame(){
+        
+        frame.add(target,"North");
+        //frame.add(submit,"South");
         frame.add(uno,"West");
         frame.add(due,"East");
         frame.add(btnPanel,"Center");
         
-        frame.setSize(300, 200);
+        frame.setSize(400, 600);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
@@ -108,6 +134,19 @@ public class GUI implements ActionListener{
                 selected = (selected +"("+String.valueOf(j)+")");
             modDue.addElement(selected);
             modUno.removeElement(uno.getSelectedValue());
+        }
+        
+        if("cerca".equals(e.getActionCommand())){
+            if(!modUno.isEmpty()){
+                modUno.removeAllElements();
+            }
+            
+            File f = new File((String)target.getText());
+            ArrayList<String> names = new ArrayList<>(Arrays.asList(f.list()));
+            for(String name:names){
+                System.out.println(name);
+                modUno.addElement(name);
+            }
         }
     }
 }
